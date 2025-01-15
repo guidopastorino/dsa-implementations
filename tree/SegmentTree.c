69d6107f8ca59ba3ad595ef1e45d5ecc3cb99233
guidopastorino <guidopasto05@gmail.com>
2025-01-14 23:08:07 -0300

    Starting Segment Tree implementation

--- tree/SegmentTree.c
+++ tree/SegmentTree.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <string.h>
+/*
+  Un Segment Tree (Árbol de Segmentos) es una estructura de datos utilizada para responder eficientemente a consultas de rango sobre un arreglo, como la suma, el valor mínimo o máximo, o la actualización de elementos en un rango. Es muy útil cuando se realizan múltiples operaciones de actualización y consulta en segmentos de un arreglo. Un Segment Tree permite realizar estas operaciones en O(log n) tiempo.
 
-int main(){
-	
+  Conceptos Clave
+    - Construcción: Se representa como un árbol binario donde cada nodo almacena información sobre un segmento del arreglo.
+    - Consultas de rango: Permite obtener información agregada (suma, mínimo, máximo, etc.) de cualquier subrango.
+    - Actualizaciones: Puede actualizar elementos individuales o rangos en O(log n).
+*/
 
-  return 0;
-}
